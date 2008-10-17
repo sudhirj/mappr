e9c17f5ed5bff863afac002f1e59323903c427a0
Sudhir Jonathan <sudhir.j@gmail.com>
2008-10-18 01:51:27 +0530

    more work - not much to say

---
+++ helpers/utils.py
@@ -0,0 +1,3 @@
+import os
+def path(p=''):
+    return os.path.join(os.path.dirname(__file__), '../'+p )
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import wsgiref.handlers,logging, presenter, os
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
+from helpers import utils
 
 class MainHandler(webapp.RequestHandler):
 
     def get(self,url=None):
         pointset = presenter.get_points_for(url)
         template_values = {'points':pointset}
-        path = "templates/index.html"
-        # path = os.path.join(os.path.dirname(__file__), '../mappr/templates/index.html')
-        self.response.out.write(template.render(path,template_values ))
+        self.response.out.write(template.render(utils.path('templates/index.html'),template_values ))
 
 def main():
     wsgiref.handlers.CGIHandler().run(createMainApplication())
