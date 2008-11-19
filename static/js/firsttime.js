7ddc835c99bb43d9c7a0e53d41825dbb294a2125
Sudhir Jonathan <sudhir.j@gmail.com>
2008-11-20 00:27:22 +0530

    minor first time correction

--- static/js/firsttime.js
+++ static/js/firsttime.js
@@ -36,6 +36,7 @@ var FirstTime = function(){
             }
             $('.error',this.dialog).text('');
             $('input#text-url',this.dialog).val(url);
+            $('#dynamic-url').text(url);
             this.dialog.dialog("open");
             $('input#text-url',this.dialog)[0].focus();
             $('#create-button').click(function(){FirstTime.save()});
